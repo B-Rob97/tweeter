1d313078f4ed537954079c50beafe2933d5f4d76
Brett Roberts <brett.roberts@live.ca>
2023-09-12 14:17:45 -0600

    feat: complete logic to reduce .counter element value for every character input - counter css color change to red on negative values

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,18 @@ $(document).ready(function() {
   
   $('#tweet-text').on('input', function() {
     const characterCount = (140 - (this.value.length));
-    console.log(characterCount);
+
+    const $parentSection = $(this).closest('.new-tweet');
+
+    const $counter = $parentSection.find('.counter');
+
+    if (characterCount < 0) {
+      $counter.css('color', '#d80000');
+      $counter.text(characterCount);
+    }
+    if (characterCount >= 0) {
+      $counter.css('color', '#4056a1');
+      $counter.text(characterCount);
+    }
   });
-
-
 });
-
-
