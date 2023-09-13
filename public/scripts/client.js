243d165ba2f21c1cce5958a0bc45dd21c2f35e7f
Brett Roberts <brett.roberts@live.ca>
2023-09-13 16:46:49 -0600

    feat: complete logic to render tweet without refresh. - Remove custom time function & use timeago.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,103 +4,90 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-let tweets = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giantssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
-    },
-    "created_at": 1694355519295
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1694441919295
-  },
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giantssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
-    },
-    "created_at": 1694355519295
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1694441919295
-  }
-];
-
-const createTweetElement = (tweet) => {
-  const { name, avatars, handle } = tweet.user;
-  const { text } = tweet.content;
-  const created_at = new Date(tweet.created_at);
-  const currentDate = new Date();
-  const timeDifference = currentDate - created_at;
-  let date;
-
-  if (timeDifference < 24 * 60 * 60 * 1000) {
-    const hours = Math.floor(timeDifference / (60 * 60 * 1000));
-    const minutes = Math.floor((timeDifference % (60 * 60 * 1000)) / (60 * 1000));
-    date = `${hours} hours ${minutes} minutes ago`;
-  } else {
-    const days = Math.floor(timeDifference / (24 * 60 * 60 * 1000));
-    date = `${days} days ago`;
-  }
-
-  return `
-    <article class="display-tweets-container">
-      <header>
-        <main class="user-profile-header">
-          <div>
-            <img src="${avatars}" alt="ProfilePic">  
-            <h2>${name}</h2>
-          </div>
-          <h2 class="user-handle">${handle}</h2>
-        </main>
-        <p class="display-tweet-text">
-          ${text}
-        </p>
-      </header>
-      <footer class="display-tweets-footer">
-        <time>
-          ${date}
-        </time>
-        <div class="tweet-icons">
-          <i class="fa-solid fa-flag"></i>
-          <i class="fa-solid fa-retweet"></i>
-          <i class="fa-solid fa-heart"></i>
-        </div>
-      </footer>
-    </article>`;
-};
-
-const renderTweets = (tweets) => {
-  $.each(tweets, (index, tweet) => {
-    $('.new-tweet').append(createTweetElement(tweet));
-  });
-};
-
 $('document').ready(() => {
-  renderTweets(tweets);
+
+  const $displayTweetsContainer = $('#new-tweet-container');
+
+  const createTweetElement = (tweet) => {
+    const { name, avatars, handle } = tweet.user;
+    const { text } = tweet.content;
+    const date = timeago.format(tweet.created_at);
+
+    return `
+      <article class="display-tweets-container">
+        <header>
+          <main class="user-profile-header">
+            <div>
+              <img src="${avatars}" alt="ProfilePic">  
+              <h2>${name}</h2>
+            </div>
+            <h2 class="user-handle">${handle}</h2>
+          </main>
+          <p class="display-tweet-text">
+            ${text}
+          </p>
+        </header>
+        <footer class="display-tweets-footer">
+          <time>
+            ${date}
+          </time>
+          <div class="tweet-icons">
+            <i class="fa-solid fa-flag"></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+          </div>
+        </footer>
+      </article>`;
+  };
+
+  const renderTweets = (tweets) => {
+    $displayTweetsContainer.empty();
+    $.each(tweets, (index, tweet) => {
+      $('#new-tweet-container').prepend(createTweetElement(tweet));
+    });
+  };
+
+  const loadTweets = () => {
+    $.ajax({
+      method: "GET",
+      url: "/tweets",
+      success: (tweets) => {
+        console.log(tweets);
+        renderTweets(tweets);
+      }
+    });
+  };
+
+  loadTweets();
+
+  const $tweetForm = $("#new-tweet-form");
+
+  $tweetForm.on('submit', (event) => {
+    event.preventDefault();
+    
+    const tweetText = $tweetForm.find('textarea[name="text"]').val();
+    
+    if (tweetText.length > 140) {
+      window.alert("Tweet is too long!");
+      return;
+    }
+
+    if (!tweetText.trim()) {
+      window.alert("Tweet content is not present!");
+      return;
+    }
+
+    const tweetData = $tweetForm.serialize();
+
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data: tweetData,
+      success: () => {
+        console.log("Post Success");
+        loadTweets();
+      }
+    });
+  });
+
 });
